edbf4e529c7c895d344b1e80c80e7761d2bb5f4d
Jay Madden <jaymaddencox@gmail.com>
2021-12-02 19:37:13 -0500

    day 2 done

--- AoC.Day2/Program.cs
+++ AoC.Day2/Program.cs
@@ -1,3 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.CompilerServices;
 
-Console.WriteLine("Hello, World!");
+namespace AoC.Day2;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var input = File.ReadAllLines("Day2.txt")
+        .Select(x => x.Split(" "))
+        .Select(y => new {command=y[0],value=int.Parse(y[1])})
+        .ToList();
+
+        var depth = 0;
+        var horizontal = 0;
+
+        foreach (var com in input)
+        {
+            if (com.command == "forward")
+            {
+                horizontal += com.value;
+            }
+            if (com.command == "up")
+            {
+                depth -= com.value;
+            }
+            if (com.command == "down")
+            {
+                depth += com.value;
+            }
+        }
+    
+        Console.WriteLine($"Value: {depth * horizontal}");
+        
+        input = File.ReadAllLines("Day2.txt")
+            .Select(x => x.Split(" "))
+            .Select(y => new {command=y[0],value=int.Parse(y[1])})
+            .ToList();
+
+        depth = 0;
+        horizontal = 0;
+        var aim = 0;
+
+        foreach (var com in input)
+        {
+            if (com.command == "forward")
+            {
+                horizontal += com.value;
+                depth += aim * com.value;
+            }
+            if (com.command == "up")
+            {
+                aim -= com.value;
+            }
+            if (com.command == "down")
+            {
+                aim += com.value;
+            }
+        }
+    
+        Console.WriteLine($"Value: {depth * horizontal}");
+    }
+}
---
+++ AoC.Day3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AoC.Day3;
+
+public static class Program
+{
+    private static string[] input;
+    
+    public static void Main()
+    {
+        input = File.ReadAllLines("Day3.txt");
+    }
+
+    public static void Part1()
+    {
+        
+    }
+    
+    public static void Part2()
+    {
+        
+    }
+}
