de795c4a0df384c022b86fcd95f203f743dff90b
Jay Madden <jaymaddencox@gmail.com>
2022-12-01 19:31:21 -0600

    2022 Day1

---
+++ AoC.2022/AoC.Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿var ansPart1 = File.ReadAllText("Day1.txt")
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(x => 
+        x.Split(Environment.NewLine)
+            .Select(int.Parse)
+            .Sum())
+    .Max();
+
+Console.WriteLine($"Part 1: {ansPart1}");
+
+var ansPart2 = File.ReadAllText("Day1.txt")
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(x => 
+        x.Split(Environment.NewLine)
+            .Select(int.Parse)
+            .Sum())
+    .OrderDescending()
+    .Take(3)
+    .Sum();
+
+Console.WriteLine($"Part 2: {ansPart2}");
