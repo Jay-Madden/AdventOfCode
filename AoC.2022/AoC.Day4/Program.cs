27d8a93eeeb1b2f0842083e8b1e2160970092a99
Jay Madden <jaymaddencox@gmail.com>
2022-12-07 13:18:29 -0600

    add day 4

---
+++ AoC.2022/AoC.Day4/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var ans1 = File.ReadAllLines("input.txt")
+    .Select(x => x.Split(",").Select(
+            y => new Range(y.Split("-").Select(int.Parse).ToList()[0], y.Split("-").Select(int.Parse).ToList()[1]))
+        .OrderByDescending(a => a.GetOffsetAndLength(3999).Length))
+    .Count(b => b.Skip(1).First().Start.Value >= b.First().Start.Value &&
+                b.Skip(1).First().End.Value <= b.First().End.Value);
+
+Console.WriteLine($"Part 1: {ans1}");
+
+var ans2 = File.ReadAllLines("input.txt")
+    .Select(x => x.Split(",").Select(
+            y => new Range(y.Split("-").Select(int.Parse).ToList()[0], y.Split("-").Select(int.Parse).ToList()[1]))
+        .OrderBy(a => a.GetOffsetAndLength(3999).Offset))
+    .Where(b => b.Skip(1).First().Start.Value <= b.First().End.Value)
+    .Count();
+
+
+Console.WriteLine($"Part 2: {ans2}");
